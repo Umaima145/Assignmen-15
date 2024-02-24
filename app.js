21048902214ae2d0f2683457fd2e9f87f2dec98a
Umaima145 <159801867+Umaima145@users.noreply.github.com>
2024-02-24 19:46:57 +0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,158 @@
+
+
+// chapter 1
+// Q1 & Q2
+
+alert("Error! Please enter a valid password.")
+ 
+//Q3
+// var a = "Welcome to Js Land "
+// var b = "Happy Coding!"
+
+alert("Welcome to Js Land \n Happy Coding! " )
+
+//Q4
+
+alert("Welcome to Js Land")
+alert("Happy Coding! ")
+
+//Q5
+
+alert("Hello... I can run JS through my web browser's console")
+
+console.log("Hello... I can run JS through my web browser's console");
+
+//chapter 2
+
+//Q1
+
+var a = "Username"
+//Q2
+var b = "Umaima Mussadiq"
+
+document.write(a + ":" + " " +b)
+
+// Q3
+var title = "Welcome to Js"
+var hello = "Hello World"
+
+alert(title)
+alert(hello)
+
+//Q4
+
+var n ="Umaima Mussadiq"
+var age = "16 years old"
+var q = "Certified Mobile App Development"
+
+alert(n)
+alert(age)
+alert(q)
+
+// Q5
+
+
+
+alert(" pizza \n pizz \n piz \n pi \n p " )
+
+
+//Q6
+
+var email = "umaimamussadiq4@gmail.com"
+alert("My email address is" + " " +email )
+
+//Q7
+var book = " A smarter way to learn JavaScript"
+alert(book)
+
+//Q8
+document.write(" <br> Yah! I can write HTML content through JavaScript")
+
+//Q9
+var d = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(d)
+document.write( "<br>" + d)
+
+
+
+//Chapter 3
+//Q1
+
+var age1 = "16years old"
+alert("I am "+ age1)
+
+//Q2
+var visit = "You have visited this site 14 times"
+alert(visit)
+
+//Q3
+var birthyear ="2007"
+document.write( "<br>"+"My birth year is " + birthyear)
+
+//Q4
+var vn = "<b>Junaid</b>"
+var pt = "<b> T-shirts(s)</b> "
+var qn = "<b> 5</b>"
+
+document.write("<br>" + vn + " odered" + qn + pt + "on Xyz Clothing Store " )
+
+
+
+
+//Chapter 1-3
+//Chapter 1
+//Q1
+alert("Umaima")
+alert("Mussadiq")
+alert("umaimamussadiq4@gmail.com")
+alert("123456667")
+alert("12345")
+
+//Q2
+alert("You're learning JS ")
+
+//Q3
+alert("Be kind to unkind people,they need it the most")
+
+//Chapter2
+//Q1
+var umaimaMussadiq;
+
+//Q2
+var h
+var h ="Hello World"
+document.write("<br>"+ h)
+
+//Q3
+var team="Web Developers team "
+alert(team)
+
+//Q4
+var bestMan = "Charlie"
+var bestMan = "Faraz"
+
+alert(bestMan)
+
+//Chapter3
+//Q1 and Q2
+
+var caseQty = 144
+
+//Q3
+var num ="9"
+
+//Q4
+var add=2+2
+alert(add)
+
+//Q5
+var merchTotal=100
+var shipping=10
+alert(merchTotal + shipping )
+
+//Q6
+
+var add1=5
+var add2= 9 + add1
+
+alert(add2)
